2ae42cbef39bb3d8a0558d97c8eef4deedc74f05
studentkrisk <krishnaearth2@gmail.com>
2024-08-09 18:32:57 -0700

    adding tokens

--- Sources/SwifTeX/SwifTeX.swift
+++ Sources/SwifTeX/SwifTeX.swift
@@ -1,20 +1,70 @@
 import SwiftUI
 
+enum CharTypes {
+    case Escape
+    case GroupStart
+    case GroupEnd
+    case MathShift
+    case Parameter
+    case Superscript
+    case Subscript
+    case Space
+    case Letter
+    case OtherChar
+    case ActiveChar
+    case CommentChar
+    case InvalidChar
+}
+
+enum Token {
+    case char(CharTypes, Character)
+    case control(String)
+}
+
 struct TeX: View {
-    let latex: String
+    
+    let toks: [Token]
     init(_ latex: String) {
-        self.latex = latex
+        let latex_array = Array(latex)
+        var cur = 0
+        while cur < latex.count {
+            let char = latex_array[cur]
+            switch char {
+            case "\\":
+                self.toks.append(Token.char(CharTypes.Escape, char))
+            case "{":
+                self.toks.append(Token.char(CharTypes.GroupStart, char))
+            case "}":
+                self.toks.append(Token.char(CharTypes.GroupEnd, char))
+            case "$":
+                self.toks.append(Token.char(CharTypes.MathShift, char))
+            case "^":
+                self.toks.append(Token.char(CharTypes.Superscript, char))
+            case "_":
+                self.toks.append(Token.char(CharTypes.Subscript, char))
+            case " ":
+                self.toks.append(Token.char(CharTypes.Space, char))
+            case Character("a"):
+                self.toks.append(Token.char(CharTypes.Space, char))
+            default:
+                print("a")
+            }
+            cur += 1
+        }
     }
     
     var body: some View {
-        Text(latex)
+        Text(toks)
     }
 }
 
 struct Example: View {
     
     var body: some View {
-        
+        List {
+            TeX(#"te{st}"#)
+            TeX(#"test"#)
+        }
     }
 }
 #Preview {
